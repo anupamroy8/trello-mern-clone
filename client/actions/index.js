e5df68aa3adbacd29aadb7ad5f0d2c72fd8d70ab
anupamroy8 <anupamroy8@gmail.com>
2020-08-20 16:17:07 +0530

    signUpSlug page added

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,31 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
+import { USER_LOGIN_SUCCESS, USER_LOGIN_FAILED, NO_TOKEN } from "../type";
 
-const rootUrl = 'http://localhost:3000/api/v1';
+const rootUrl = "http://localhost:3000/api/v1";
 
 const setTokenToAxios = (token) => {
-  const newToken = localStorage.getItem('authToken') || '';
+  const newToken = localStorage.getItem("authToken") || "";
   axios.defaults.headers.Authorization = newToken;
-}
+};
+
+export const userSignUp = (data, history) => {
+  console.log(data);
+  return function (dispatch) {
+    axios
+      .post(`${url}`, { user: data })
+      .then((res) => {
+        history.push("/login");
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const userLogin = (data, history) => {
+  return function (dispatch) {
+    axios.post(`${url}/login`, { user: data }).then((user) => {
+      console.log(user, "user");
+      return dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: user.data,
+      });
+      localStorage.setItem("token", user.data.token), history.push, "/";
+    });
+  };
+};
 
 export const getCurrentUser = () => {
-  return (dispatch => {
-    axios.get(`${rootUrl}/users/me`)
-    .then(res => {
-      dispatch({
-        type: 'USER_LOGIN_SUCCESS',
-        data: res.data
+  return (dispatch) => {
+    axios
+      .get(`${rootUrl}/users/me`)
+      .then((res) => {
+        dispatch({
+          type: "USER_LOGIN_SUCCESS",
+          data: res.data,
+        });
       })
-    })
-    .catch(err => {
-      dispatch({type: 'USER_LOGIN_FAILED'})
-    })
-  })
-}
+      .catch((err) => {
+        dispatch({ type: "USER_LOGIN_FAILED" });
+      });
+  };
+};
 
 export const noToken = () => {
-  return(dispatch => {
+  return (dispatch) => {
     dispatch({
-      type: 'NO_TOKEN'
-    })
-  })
-}
+      type: "NO_TOKEN",
+    });
+  };
+};
--- client/components/SignUpSlug.js
+++ client/components/SignUpSlug.js
@@ -1,72 +1,72 @@
 import React from "react";
 import Logo from "../../public/images/trello-logo-blue.svg";
-import { NavLink } from "react-router-dom";
-import { connect } from "mongoose";
-import { userLogin } from "../actions";
+import { userSignUp } from "../actions/index";
+import { NavLink, Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-class Login extends React.Component {
+class SignUp extends React.Component {
   state = {
     email: "",
     password: "",
+    username: "",
   };
   handleInput = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  handleSubmit = (e) => {
-    return this.props.dispatch(userLogin(this.state, this.props.history));
+
+  handleSubmit = () => {
+    return this.props.dispatch(userSignUp(this.state, this.props.history));
   };
   render() {
-    const { email, password } = this.state;
+    const { email, password, username } = this.state;
     return (
       <div className="signUp-form">
         <section>
           <div className="signUp-logo">
-            <NavLink to="/">
-              <img src={Logo} alt="logo" />
-            </NavLink>
+            <img src={Logo} alt="logo" />
           </div>
           <div className="form-wrapper">
             <div className="form-box">
-              <h2>Log in to Trello</h2>
+              <h2>Sign up for your account</h2>
               <input
                 className="signUp-form-input login-form-input"
                 type="email"
-                placeholder="Enter email"
-                value={email}
+                placeholder="email"
                 name="email"
+                value={email}
+                onChange={(e) => this.handleInput(e)}
+              />
+              <input
+                className="signUp-form-input login-form-input"
+                type="text"
+                placeholder="Enter full name"
+                value={username}
+                name="username"
                 onChange={(e) => this.handleInput(e)}
               />
               <input
                 className="signUp-form-input login-form-input"
                 type="password"
-                placeholder="Enter password"
+                placeholder="Create password"
                 value={password}
                 name="password"
                 onChange={(e) => this.handleInput(e)}
               />
+              <p>
+                By signing up, you confirm that you've read and accepted our
+                Terms of Service and Privacy Policy.
+              </p>
               <button
                 type="submit"
                 onClick={this.handleSubmit}
-                className="form-btn login-btn"
+                className="form-btn "
               >
-                Log In
-              </button>
-              <div className="or">
-                <span>OR</span>
-              </div>
-              <button type="submit" className="form-btn auth-btn">
-                Login with Google
-              </button>
-              <button type="submit" className="form-btn auth-btn">
-                Continue with Microsoft
-              </button>
-              <button type="submit" className="form-btn auth-btn">
-                Continue with Apple
+                Continue
               </button>
               <hr />
               <div className="login-link">
-                <NavLink className="signUp-link" to="/signUp">
-                  Can't log in? Sign up for an account
+                <NavLink className="signUp-link" to="/login">
+                  Already have an account? Log In
                 </NavLink>
               </div>
             </div>
@@ -81,4 +81,4 @@ function mapStateToProps(state) {
   return { currentUser: state.currentUser };
 }
 
-export default connect(mapStateToProps)(Login);
+export default connect(mapStateToProps)(SignUpSlug);
